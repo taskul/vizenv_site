aa5ad5fe10d2c8fe15472c1520e8eb854646cd60
Tastan Kulmeshkenov <tastankulmeshkenov@gmail.com>
2024-06-21 21:38:32 -0500

    added demo link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,9 +24,11 @@ const NavBar = ({ onAboutClick, onDownloadClick }) => {
       <ul>
         <li><a href="#about" onClick={onAboutClick}>About</a></li>
         <li><a href="#download" onClick={onDownloadClick}>Download</a></li>
+        <li><a href="https://youtu.be/QKxO1FUUlJo">Demo</a></li>
       </ul>
     </nav>
   );
 };
+
 
 export default NavBar;
