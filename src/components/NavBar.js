d67007a7f334310fe3b9c3277eb2f5a8e8b13516
Tastan Kulmeshkenov <tastankulmeshkenov@gmail.com>
2024-09-13 22:59:13 -0500

    added author link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import './NavBar.css';
-
-// const NavBar = () => {
-//   return (
-//     <nav className="navbar">
-//       <ul>
-//         <li><a href="https://github.com/your-repo" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-//         <li><a href="/docs">Docs</a></li>
-//         <li><a href="/about">About</a></li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default NavBar;
-
 import React from 'react';
 import './NavBar.css';
 
--- src/components/ThirdContainer.js
+++ src/components/ThirdContainer.js
@@ -9,6 +9,8 @@ const ThirdContainer = () => {
         Our beta version is now available for download. Click the link below to get started!
       </p>
       <a href="https://github.com/taskul/vizenv/releases/tag/v1.0.0-beta-b" className='download-button'>Download VizEnv beta</a>
+
+      <a href="https://taskul.xyz/" className='secondary-button'>Author</a>
     </div>
   );
 };
